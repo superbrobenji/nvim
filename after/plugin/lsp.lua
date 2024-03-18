43105109d10c2fb20c1a4ec69f2e9ff8e62afe6f
superbrobenji <bennie.h.swanepoel@gmail.com>
2024-03-18 13:47:12 +0200

    removed unsuported line

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -129,7 +129,6 @@ lsp.on_attach(function(client, bufnr)
     end
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
